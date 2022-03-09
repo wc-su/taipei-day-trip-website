637e71ab84025b08675c1020759fd16203e1142f
wc-su <theblissout@gmail.com>
2022-03-09 13:06:29 +0800

    1.配合html/css名稱變更調整程式。2.移除未使用變數nowPage。3.增加API回傳資訊判斷，顯示不同訊息。

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,4 @@
 // 頁數
-let nowPage = 0; // 目前頁數，預設是 0
 let nextPage = 0; // 下一頁頁數，預設是 0，表示未載入資訊
 // 註記
 let dataLoading = false; // 資料是否正在讀取，預設為 false
@@ -7,10 +6,10 @@ let hasSearchText = false; // 是否有輸入關鍵字，預設為 false
 let typeChinese = false; // 是否正在輸入中文，預設為 false
 // 旅遊景點
 const attractions = document.querySelector(".attractions");
-const attractions_observer = document.querySelector(".attractions-observer");
+const attractions_observer = document.querySelector(".attractions__observer");
 // 搜尋事件
-const searchText = document.querySelector(".search-text");
-const searchBtn = document.querySelector(".search-btn");
+const searchText = document.querySelector(".search-container__text");
+const searchBtn = document.querySelector(".search-container__btn");
 
 // 條件達成做什麼：符合設定條件下，目標進入或離開 viewport 時觸發此 callback 函式
 const observerCallback = ([entry]) => {
@@ -19,9 +18,8 @@ const observerCallback = ([entry]) => {
     }
 };
 // 建立一個 intersection observer，帶入相關設定資訊
-// 觸發條件都使用預設即可，所以不特別帶入(第二個參數)
+// 觸發條件都使用預設，不帶入觸發條件(第二個參數)
 const observer = new IntersectionObserver(observerCallback);
-
 
 function checkInput() {
     // 資料載入中，不做任何處理
@@ -46,53 +44,77 @@ function getAttractions(url) {
     .then((response) => {
         return response.json()
     }).then((data) => {
-        // 更新畫面
-        renderAttractions(data.data);
-        // 更新
-        if(data.nextPage === null) {
-            attractions_observer.style.display = "none";
-            observer.unobserve(attractions_observer);
-        } else {
-            // 更新目前頁數
-            nowPage = nextPage;
-        }
-        // 更新下一頁頁數
-        nextPage = data.nextPage;
+        renderAttractions(data);
         // 資料讀取結束
         dataLoading = false;
     });
 }
 
-function renderAttractions(attractionsData) {
-    attractionsData.forEach(attraction => {
-        // 新增旅遊景點區塊
-        const attractionWrap = document.createElement("li");
-        attractionWrap.classList.add("attraction-wrap");
-        // 旅遊景點圖片
-        const attractionImg = document.createElement("img");
-        // attractionImg.src = attraction.images[0];
-        attractionImg.style.backgroundImage = `url(${attraction.images[0]}`;
-        attractionImg.classList.add("attraction-img");
-        // 旅遊景點名稱
-        const attractionTitle = document.createElement("div");
-        attractionTitle.textContent = attraction.name;
-        attractionTitle.classList.add("attraction-title");
-        // 旅遊景點捷運站
-        const attractionMrt = document.createElement("div");
-        attractionMrt.textContent = attraction.mrt;
-        attractionMrt.classList.add("attraction-mrt");
-        // 旅遊景點捷分類
-        const attractionCategory = document.createElement("div");
-        attractionCategory.textContent = attraction.category;
-        attractionCategory.classList.add("attraction-category");
-        // 將新增內容加入區塊中
-        attractionWrap.appendChild(attractionImg);
-        attractionWrap.appendChild(attractionTitle);
-        attractionWrap.appendChild(attractionMrt);
-        attractionWrap.appendChild(attractionCategory);
-        // 景點區塊加入畫面
-        attractions.appendChild(attractionWrap);
-    });
+function renderAttractions(renderData) {
+    let msg = null;
+    // 取得旅遊景點資料
+    attractionsData = renderData.data;
+    
+    // 檢核 API 回傳
+    if(renderData.error) {
+        // 回傳失敗，帶出回傳訊息
+        msg = renderData.message;
+    } else if(attractionsData.length == 0) {
+        // 無資料
+        msg = "查無旅遊景資訊";
+    }
+
+    // 顯示在畫面上
+    if(msg != null) {
+        // 顯示訊息
+        const attractionMsg = document.createElement("p");
+        attractionMsg.classList.add("attraction-msg");
+        attractionMsg.textContent = msg;
+        attractions.appendChild(attractionMsg);
+        attractions.classList.add("attractions--nodata");
+    } else {
+        attractionsData.forEach(attraction => {
+            // 新增旅遊景點區塊
+            const attractionWrap = document.createElement("li");
+            attractionWrap.classList.add("attraction-wrap");
+            // 旅遊景點圖片
+            const attractionImg = document.createElement("img");
+            attractionImg.style.backgroundImage = `url(${attraction.images[0]}`;
+            attractionImg.classList.add("attraction-img");
+            // 旅遊景點名稱
+            const attractionTitle = document.createElement("div");
+            attractionTitle.textContent = attraction.name;
+            attractionTitle.classList.add("attraction-title");
+            // 旅遊景點捷運站
+            const attractionMrt = document.createElement("div");
+            attractionMrt.textContent = attraction.mrt;
+            attractionMrt.classList.add("attraction-mrt");
+            // 旅遊景點捷分類
+            const attractionCategory = document.createElement("div");
+            attractionCategory.textContent = attraction.category;
+            attractionCategory.classList.add("attraction-category");
+            // 將新增內容加入區塊中
+            attractionWrap.appendChild(attractionImg);
+            attractionWrap.appendChild(attractionTitle);
+            attractionWrap.appendChild(attractionMrt);
+            attractionWrap.appendChild(attractionCategory);
+            // 景點區塊加入畫面
+            attractions.appendChild(attractionWrap);
+        });
+        attractions.classList.remove("attractions--nodata");
+        if(renderData.nextPage === null) {
+            msg = "notHasNext";
+        }
+        // 更新下一頁頁數
+        nextPage = renderData.nextPage;
+    }
+    
+    if(msg != null) {
+        // 觀察物件不顯示在畫面並將動畫取消，不再觀察
+        attractions_observer.style.display = "none";
+        attractions_observer.classList.remove("attractions__observer--animation");
+        observer.unobserve(attractions_observer);
+    }
 }
 
 // 頁面初始
@@ -104,11 +126,12 @@ document.addEventListener("DOMContentLoaded", () => {
 // 關鍵字搜尋事件
 searchBtn.addEventListener("click", (e) => {
     // 初始化
-    nowPage = 0;
     nextPage = 0;
     dataLoading = false;
     attractions.innerHTML = "";
+    // 將觀察物件顯示在畫面上，增加動畫效果
     attractions_observer.style.display = "block";
+    attractions_observer.classList.add("attractions__observer--animation");
     // 設定
     hasSearchText = true;
     observer.observe(attractions_observer);
@@ -117,7 +140,6 @@ searchBtn.addEventListener("click", (e) => {
 // 查詢功能優化
 searchText.addEventListener("keyup", (e) => {
     if (!typeChinese && e.keyCode === 13) {
-        console.log("click search btn");
         searchBtn.click();
     }
 });
@@ -125,11 +147,9 @@ searchText.addEventListener("keydown", (e) => {
     // 開始輸入中文
     if (!typeChinese && e.keyCode === 229) {
         typeChinese = true;
-        console.log("input chinese start", typeChinese);
     }
     // 結束輸入中文
     if (typeChinese && e.keyCode === 13) {
         typeChinese = false;
-        console.log("input chinese end", typeChinese);
     }
 });
