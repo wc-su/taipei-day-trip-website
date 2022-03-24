0a753fb85f88aa2bfa507b61275e1056bfda21f4
wc-su <theblissout@gmail.com>
2022-03-24 19:48:39 +0800

    調整寫入sys.path路徑方式

--- data/initData.py
+++ data/initData.py
@@ -1,6 +1,5 @@
 import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append("../")
 
 import json
 from routes.config import Config
--- data/initUser.py
+++ data/initUser.py
@@ -1,6 +1,5 @@
 import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append("../")
 
 from routes.config import Config
 from models.connectDB import DBModel
